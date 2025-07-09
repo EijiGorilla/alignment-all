eac8119f55deb1a361b83b083e551c835d1ba53c
EijiGorilla <emasuz@hotmail.com>
2025-07-09 09:47:17 +0800

    updated

---
+++ src/components/AsOfDatePanel.tsx
@@ -0,0 +1,23 @@
+function AsOfDatePanel() {
+  return (
+    <>
+      <div
+        style={{
+          color: "rgb(220, 220, 220)",
+          fontSize: "0.8rem",
+          float: "right",
+          marginRight: "5px",
+          marginTop: "5px",
+          zIndex: "1",
+          position: "fixed",
+          bottom: 5,
+          left: "20px",
+        }}
+      >
+        As of June 2025
+      </div>
+    </>
+  );
+}
+
+export default AsOfDatePanel;
